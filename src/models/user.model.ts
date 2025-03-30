d6407867a47c0c3118b35aab2736cd96b81e869b
Geeth Wishkamal <41125548+geethwish@users.noreply.github.com>
2025-03-31 00:00:01 +0530

    name not saving issue fixed

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -9,6 +9,7 @@ export interface UserDocument extends Document {
 }
 
 const UserSchema: Schema = new Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
 });
